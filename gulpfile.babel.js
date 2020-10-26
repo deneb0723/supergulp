1988973da5cffe87e75f52f9e6dd88117b98c4e3
denev <65989857+deneb0723@users.noreply.github.com>
2020-10-26 23:39:30 +0900

    gulp img, scss, autoprefixer, csso 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,12 +2,27 @@ import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
 import ws from "gulp-webserver";
+import gimage from "gulp-image";
+import sass from "gulp-sass";
+import autoprefixer from "gulp-autoprefixer";
+import miniCss from "gulp-csso";
+
+sass.compiler = require("node-sass");
 
 const routes = {
   pug: {
     watch: "src/**/*.pug",
     src: "src/*.pug",
     dest: "build"
+  },
+  img: {
+    src: "src/img/*",
+    dest: "build/img"
+  },
+  scss: {
+    watch: "src/scss/**/*.scss",
+    src: "src/scss/styles.scss",
+    dest: "build/css"
   }
 };
 
@@ -17,16 +32,34 @@ const pug = () =>
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
+const img = () => 
+  gulp
+    .src(routes.img.src)
+    .pipe(gimage())
+    .pipe(gulp.dest(routes.img.dest));
+
+const styles = () => 
+  gulp
+    .src(routes.scss.src)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(miniCss())
+    .pipe(gulp.dest(routes.scss.dest))
+
+
 const clean = () => del(["build"]);
 
-const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true}));
+const webserver = () => gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
-const watch = () => gulp.watch(routes.pug.watch, pug);
+const watch = () => {
+  gulp.watch(routes.pug.watch, pug)
+  gulp.watch(routes.img.src, img)
+  gulp.watch(routes.scss.watch, styles)};
 
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug]);
+const assets = gulp.series([pug, styles]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const dev = gulp.series([prepare, assets, live]);
